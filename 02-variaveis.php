a4e81631178e540c26f781b4bf2d5767ed52686f
victor-grandmasterwitcher <victortavamaral@gmail.com>
2021-09-21 17:39:14 -0300

    Parei em Curso de PHP 7 - Aula 07 - Variáveis dinâmicas

---
+++ 02-variaveis.php
@@ -0,0 +1,35 @@
+<?php
+//Meus dados
+$nome = "Victor Amaral";
+$idade = 19;
+$altura = 1.70;
+
+echo "Meu nome é $nome, minha idade é $idade, e tenho $altura m de altura <br>";
+
+$nome = "Clebinho Leite";
+$idade = 23;
+$altura = 1.56;
+
+echo "Meu nome é $nome, minha idade é $idade, e tenho $altura m de altura <br>";
+echo "<hr>";
+
+/* 
+- Nomes válidos para var
+$nome
+$_meunome
+$carro10
+$numero1
+$meu_nome
+$meuNome
+
+- Nomes inválidos
+$123nome
+$carro*@!#
+$meu carro
+*/
+
+$carro = "Hylux";
+$Carro = "Fusca";
+
+echo $carro; // Hylux
+echo $Carro; // Fusca
